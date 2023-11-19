d7b9c78947947a671be9ad2796f02af83a4164c9
postym <mitchmalto726@gmail.com>
2023-11-19 20:53:06 +0800

    Added Profile Info Show

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\CarouselItemsController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Models\CarouselItems;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout');
 
+    // Admin APIs
 Route::controller(CarouselItemsController::class)->group(function(){
     Route::get('/carousel',  'index');
     Route::get('/carousel/{id}', 'show');
@@ -36,7 +38,6 @@ Route::controller(CarouselItemsController::class)->group(function(){
     Route::post('/carousel','store');
     Route::put('/carousel/{id}','update');
 });
-
     // Route::controller(CarouselItemsController::class)->group(function(){
     // Route::get('/carousel',  'index');
     // Route::get('/carousel/{id}', 'show');
@@ -44,17 +45,19 @@ Route::controller(CarouselItemsController::class)->group(function(){
     // Route::post('/carousel','store');
     // Route::put('/carousel/{id}','update');
     // });
-
     Route::controller(UserController::class)->group(function(){
     Route::get('/user', 'index' );
     Route::delete('/user/{id}','destroy');
     Route::get('/user/{id}', 'show');
-    
     Route::put('/user/{id}','update')->name('user.update');
     Route::put('/user/email/{id}','email')->name('user.email');
     Route::put('/user/password/{id}', 'password')->name('user.password');
     Route::put('/user/image/{id}', 'image')->name('user.image');
 });
+
+// User Specific APIs
+Route::put('/profile/image', [ProfileController::class, 'image'])->name('profile.image');
+Route::get('/profile/show', [ProfileController::class, 'show']);
 
 
 
